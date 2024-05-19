a4135598dd5dea2806cf6c9c9d61b58bebddd84d
BD103 <59022059+BD103@users.noreply.github.com>
2024-05-19 17:17:51 -0400

    refactor: format

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/Exceptions.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/Exceptions.kt
@@ -4,8 +4,10 @@ import kotlin.reflect.KClass
 
 class InitAPIMoreThanOnce(clazz: KClass<out API>) : IllegalStateException("Tried to initialize an API ${clazz.simpleName} more than once.")
 
-class InitLinearAPIWithoutLinearOpMode(clazz: KClass<out API>) : IllegalArgumentException("""
+class InitLinearAPIWithoutLinearOpMode(clazz: KClass<out API>) : IllegalArgumentException(
+    """
     Tried to initialize a linear API ${clazz.simpleName} without a `LinearOpMode`.
     Please make sure that your opmode extends `LinearOpMode`.
     Also please check the ${clazz.simpleName} has not accidentally set `isLinear` to true.
-""".trimIndent())
+    """.trimIndent(),
+)
