14a4542d66009994463f50d527ba05c645320aa8
BD103 <59022059+BD103@users.noreply.github.com>
2024-05-30 19:16:03 -0400

    feat: add content to opmodes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoMain.kt
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.autonomous
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+
+/**
+ * This is the main code for the autonomous segment of the game.
+ */
+@Autonomous(name = "Main")
+class AutoMain : LinearOpMode() {
+    override fun runOpMode() {
+        // Initialization
+
+        waitForStart()
+
+        // Start
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/TeleOpMain.kt
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.teleOp
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+
+/**
+ * This is the main code for the driver-controlled segment of the game.
+ */
+@TeleOp(name = "Main")
+class TeleOpMain : OpMode() {
+    override fun init() {}
+    override fun loop() {}
+}
