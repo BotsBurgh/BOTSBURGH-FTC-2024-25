807198d5ea93a0997f99bf94d70270ef2d041175
cadence <117321183+cadenceforney@users.noreply.github.com>
2024-06-05 20:39:40 -0400

    Update RobotConfig.kt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/RobotConfig.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/RobotConfig.kt
@@ -13,25 +13,27 @@ import org.firstinspires.ftc.teamcode.utils.RobotConfig.debug
  * also modify the code!** The configuration can also change during initialization depending on
  * various build constants like [debug].
  */
-
 object RobotConfig {
+
     /**
      * When true, enables debugging features like camera streaming and more logs.
      *
      * This should be disabled for competitions.
      */
-    const val debug: Boolean = true
+     const val debug: Boolean = true
 
     /**
      * Creates a string representing the current robot build constants.
      */
-    override fun toString() = "RobotConfig(debug=$debug)"
-
-    /** Example on how to set up a Config Variable
+     override fun toString() = "RobotConfig(debug=$debug)"
+    /** Example on how to set up a Config Variable.
     @Config
     object NAMEOFOBJECT {
-    @JvmField
-    var NAMEOFVARIABLE: Double = 145.1
+        /**
+         * This is what the variable does.
+        */
+        @JvmField
+        var NAMEOFVARIABLE: Double = 145.1
     }
      */
 }
