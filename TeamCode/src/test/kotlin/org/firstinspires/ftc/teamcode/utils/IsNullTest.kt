5585fa8e1b1ee24ef6cc1e542553dc9938bfdbab
BD103 <59022059+BD103@users.noreply.github.com>
2024-06-13 20:39:44 -0400

    feat: is null tests

---
+++ TeamCode/src/test/kotlin/org/firstinspires/ftc/teamcode/utils/IsNullTest.kt
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.utils
+
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+
+internal class IsNullTest {
+    private val a: Int? = null
+    private val b: Int? = 10
+
+    @Test
+    fun testIsNull() {
+        assertTrue(a.isNull())
+        assertFalse(b.isNull())
+    }
+
+    @Test
+    fun testIsNotNull() {
+        assertFalse(a.isNotNull())
+        assertTrue(b.isNotNull())
+    }
+}
