8deee4a91e3566fd6c84c5ec06e26440708308b0
BD103 <59022059+BD103@users.noreply.github.com>
2024-06-13 20:49:12 -0400

    Create `API` class (#13)
    Closes #4.
    
    - [x] Access the current OpMode without passing the object into every
    function.
    - [x] Support running code when an OpMode is initialized.
    - [x] ~~Support running code when an OpMode is stopped.~~
      - Skipped for now, will be implemented if needed.
    - [x] Access LinearOpMode-specific functions, with possible fallbacks /
    errors if unavailable.
    - [x] Specify API dependencies.
    - [x] Reset variables so old configuration does not persist across runs.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/API.kt
@@ -0,0 +1,86 @@
+package org.firstinspires.ftc.teamcode.core
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import org.firstinspires.ftc.teamcode.utils.isNotNull
+
+/**
+ * An API is shared code used by [OpMode]s.
+ *
+ * APIs are used to abstract over common tasks, such as controlling the wheels, that can be shared
+ * by multiple [OpMode]s. The must be initialized by the [OpMode] that uses them.
+ */
+abstract class API {
+    private var nullableOpMode: OpMode? by Resettable { null }
+
+    /**
+     * Accesses the current [OpMode].
+     *
+     * This is a `protected` value, so only the API itself will be able to access this.
+     *
+     * @throws APINotInitialized If [init] is not called first.
+     */
+    protected val opMode: OpMode
+        get() = this.nullableOpMode ?: throw APINotInitialized(this::class)
+
+    /**
+     * Accesses the current [OpMode] as a [LinearOpMode].
+     *
+     * @throws IllegalLinearOpModeAccess If [isLinear] is false.
+     * @throws APINotInitialized If [init] is not called first.
+     */
+    protected val linearOpMode: LinearOpMode
+        get() {
+            if (!this.isLinear) {
+                throw IllegalLinearOpModeAccess(this::class)
+            }
+
+            return this.opMode as LinearOpMode
+        }
+
+    /**
+     * Defines whether this API requires features from [LinearOpMode].
+     *
+     * If set to true, [init] can only be called from within a [LinearOpMode]. This is useful if you
+     * require methods such as [LinearOpMode.opModeInInit] or [LinearOpMode.sleep].
+     *
+     * @see linearOpMode
+     */
+    open val isLinear: Boolean = false
+
+    /**
+     * Configures all APIs that must be initialized in order to use this API.
+     *
+     * If dependencies are not initialized, [MissingDependency] will be thrown when the start button
+     * is pressed.
+     */
+    open val dependencies: Set<API> = emptySet()
+
+    /**
+     * Initializes this API.
+     *
+     * # In [OpMode] and [LinearOpMode]
+     *
+     * Call [init] during the initialization stage.
+     *
+     * # In [API]
+     *
+     * Override this function to run custom initialization, but make sure to call
+     * `super.init(opMode)` or the program will crash.
+     */
+    open fun init(opMode: OpMode) {
+        // You can only initialize an API once without tearing it down.
+        if (this.nullableOpMode.isNotNull()) {
+            throw InitAPIMoreThanOnce(this::class)
+        }
+
+        // A LinearOpMode must be passed into this function if `isLinear` is true.
+        if (this.isLinear && opMode !is LinearOpMode) {
+            throw InitLinearAPIWithoutLinearOpMode(this::class)
+        }
+
+        this.nullableOpMode = opMode
+
+        Dependencies.registerAPI(this)
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/Dependencies.kt
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode.core
+
+import android.content.Context
+import com.qualcomm.ftccommon.FtcEventLoop
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.OpModeManagerNotifier
+import org.firstinspires.ftc.ftccommon.external.OnCreateEventLoop
+
+/**
+ * A listener that ensures API dependencies are properly initialized when the start button is
+ * pressed.
+ */
+object Dependencies : OpModeManagerNotifier.Notifications {
+    private val initializedAPIs: MutableSet<API> by Resettable { mutableSetOf() }
+
+    override fun onOpModePreInit(opMode: OpMode) {}
+
+    override fun onOpModePreStart(opMode: OpMode) {
+        this.checkDependencies()
+    }
+
+    override fun onOpModePostStop(opMode: OpMode) {}
+
+    @OnCreateEventLoop
+    @JvmStatic
+    fun register(
+        @Suppress("UNUSED_PARAMETER")
+        context: Context,
+        ftcEventLoop: FtcEventLoop,
+    ) {
+        ftcEventLoop.opModeManager.registerListener(this)
+    }
+
+    /**
+     * Registers an API as initialized.
+     *
+     * This is called within the super implementation of [API.init], and should never be called
+     * manually.
+     */
+    internal fun registerAPI(api: API) {
+        this.initializedAPIs.add(api)
+    }
+
+    private fun checkDependencies() {
+        for (api in this.initializedAPIs) {
+            for (dep in api.dependencies) {
+                if (!this.initializedAPIs.contains(dep)) {
+                    throw MissingDependency(api::class, dep::class, this.initializedAPIs)
+                }
+            }
+        }
+    }
+}
