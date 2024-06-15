5d9802f8b3f9c6dfa3614f5f7fc2ef2267acd461
BD103 <59022059+BD103@users.noreply.github.com>
2024-06-15 16:30:00 -0400

    feat: test normal usage of api

--- TeamCode/src/test/kotlin/org/firstinspires/ftc/teamcode/core/APITest.kt
+++ TeamCode/src/test/kotlin/org/firstinspires/ftc/teamcode/core/APITest.kt
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode.core
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import kotlin.test.Test
 import kotlin.test.assertFailsWith
+import kotlin.test.assertSame
 
 class APITest {
     private open class EmptyAPI : API() {
@@ -19,6 +21,31 @@ class APITest {
         override fun init() {}
 
         override fun loop() {}
+    }
+
+    private class EmptyLinearOpMode : LinearOpMode() {
+        override fun runOpMode() {}
+    }
+
+    @Test
+    fun testAPI() {
+        val api = EmptyAPI()
+        val opMode = EmptyOpMode()
+
+        api.init(opMode)
+
+        assertSame(opMode, api.accessOpMode())
+    }
+
+    @Test
+    fun testLinearAPI() {
+        val api = EmptyLinearAPI()
+        val linearOpMode = EmptyLinearOpMode()
+
+        api.init(linearOpMode)
+
+        assertSame(linearOpMode, api.accessOpMode())
+        assertSame(linearOpMode, api.accessLinearOpMode())
     }
 
     @Test
