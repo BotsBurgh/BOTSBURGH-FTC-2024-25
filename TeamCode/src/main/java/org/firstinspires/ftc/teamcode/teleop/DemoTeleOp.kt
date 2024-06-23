24631df824ed6ff8edc8c2aaccc9f750a2654c9c
cadence <117321183+cadenceforney@users.noreply.github.com>
2024-06-23 17:13:19 -0400

    Demo TeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/DemoTeleOp.kt
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.opmode.teleop
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.firstinspires.ftc.teamcode.api.DemoAPI
+
+@TeleOp(name = "Demo TeleOp")
+@Disabled
+class DemoTeleOp : OpMode() {
+    // Run once, when "Init" is pressed on driver hub.
+    override fun init() {
+
+        // Initializes an API, shared code used by components.
+        DemoAPI.init(this)
+    }
+
+    // Run repeatedly while the robot is running.
+    override fun loop() {
+        val x = this.gamepad1.left_stick_x.toDouble()
+        val y = this.gamepad1.left_stick_y.toDouble()
+        val rotation = this.gamepad1.right_stick_x.toDouble()
+
+        // Strafe + rotate robot using left and right joysticks.
+        DemoAPI.drive(
+            x - y + rotation,
+            -x - y - rotation,
+            -x - y + rotation,
+            x - y - rotation,
+        )
+    }
+}
