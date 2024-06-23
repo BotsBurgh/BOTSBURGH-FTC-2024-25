a8941b428c61d3fa9366baabd51ae0ff3db1a6f6
BD103 <59022059+BD103@users.noreply.github.com>
2024-06-23 15:21:29 -0400

    feat: begin turn action

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/KiwiDrive.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/KiwiDrive.kt
@@ -1,16 +1,20 @@
 package org.firstinspires.ftc.teamcode.roadrunner
 
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket
 import com.acmerobotics.roadrunner.AccelConstraint
+import com.acmerobotics.roadrunner.Action
 import com.acmerobotics.roadrunner.AngularVelConstraint
 import com.acmerobotics.roadrunner.MinVelConstraint
 import com.acmerobotics.roadrunner.Pose2d
 import com.acmerobotics.roadrunner.ProfileAccelConstraint
 import com.acmerobotics.roadrunner.ProfileParams
+import com.acmerobotics.roadrunner.TimeTurn
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder
 import com.acmerobotics.roadrunner.TrajectoryBuilderParams
 import com.acmerobotics.roadrunner.TranslationalVelConstraint
 import com.acmerobotics.roadrunner.TurnConstraints
 import com.acmerobotics.roadrunner.VelConstraint
+import com.acmerobotics.roadrunner.now
 import kotlin.math.PI
 
 class KiwiDrive(config: Config, private val pos: Pose2d) {
@@ -26,6 +30,49 @@ class KiwiDrive(config: Config, private val pos: Pose2d) {
         /** Maximum angular acceleration, in rad/s^2. */
         val maxAngAccel: Double = PI,
     )
+
+    class TurnAction(private val turn: TimeTurn) : Action {
+        /**
+         * When this action was first run.
+         *
+         * This is negative if the action has not been initialized.
+         */
+        private var start: Double = -1.0
+
+        override fun run(p: TelemetryPacket): Boolean {
+            val elapsed =
+                if (start < 0.0) {
+                    // First time running, initialize start.
+                    start = now()
+                    0.0
+                } else {
+                    // Calculate elapsed time.
+                    now() - start
+                }
+
+            // TODO: Exit condition after certain amount of time has passed, stopping wheels.
+
+            val target = this.turn[elapsed]
+            // TODO: Writer shenanigans.
+
+            // TODO: Kiwi controller command.
+            // TODO: Writer shenanigans.
+
+            // TODO: Inverse command for wheel velocities.
+            // TODO: Get voltage.
+
+            // TODO: Create feed-forward.
+
+            // TODO: Calculate wheel power.
+            // TODO: Writer shenanigans.
+
+            // TODO: Set wheel power.
+
+            // TODO: Draw to FTC Dashboard.
+
+            return true
+        }
+    }
 
     // Values copied from `MecanumDrive`.
     private val trajectoryBuilderParams =
