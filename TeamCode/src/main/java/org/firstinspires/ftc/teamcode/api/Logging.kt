205a58a09fee54a903c04b6fabec930225e7cecb
Vifi5 <117041326+Vif15@users.noreply.github.com>
2024-06-28 11:38:45 -0400

    Feat: Made functions createFile and writeFile

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/Logging.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/Logging.kt
@@ -1,15 +1,22 @@
 package org.firstinspires.ftc.teamcode.api
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil
 import org.firstinspires.ftc.teamcode.core.API
-import org.firstinspires.ftc.teamcode.utils.RobotConfig
+import java.io.BufferedWriter
 import java.io.File
 import java.io.FileWriter
 
 object Logging: API() {
 
+    lateinit var logFile: File
+        private set
+    lateinit var logWriter: BufferedWriter
+        private set
+
     override fun init(opMode: OpMode) {
         super.init(opMode)
+
 
 //        if (RobotConfig.debug) {
 //
@@ -17,8 +24,33 @@ object Logging: API() {
         //@TODO delete old files
     }
 
-    fun createFile(fileName: String) {
-
+    val BOTSBURGH_FOLDER: File by lazy {
+        val res = File(AppUtil.ROOT_FOLDER, "/BotsBurgh/")
+        res.mkdirs()
+        res
     }
+
+    /**
+     *
+     */
+    fun createFile(fileName: String) {
+        logFile = File(BOTSBURGH_FOLDER, "/$fileName.csv")
+        logWriter = BufferedWriter(FileWriter(logFile))
+        logFile.createNewFile()
+    }
+
+    fun writeFile(data: Double) {
+        logWriter.write("$data")
+        logWriter.write(opMode.runtime.toString())
+        logWriter.newLine()
+    }
+
+    fun writeFile(data: Array<Double>) {
+        for (i in data) logWriter.write(i.toString())
+
+        logWriter.write(opMode.runtime.toString())
+        logWriter.newLine()
+    }
+
 
 }
