d2735f1fcb522589e28f1878aeedeed93c7d28e4
Imthebiggestbird2 <joshbirendra@gmail.com>
2024-07-01 21:44:07 -0400

    Fix for dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.kotlinter) apply false
-    id("org.jetbrains.dokka") version "1.9.20"
 }
 
 // This is now required because aapt2 has to be downloaded from the
