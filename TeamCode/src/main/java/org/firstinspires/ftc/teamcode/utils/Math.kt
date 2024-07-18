93a891589104b47140533fe60d75c95b8fa10266
BD103 <59022059+BD103@users.noreply.github.com>
2024-07-18 15:10:49 -0400

    `Polar2dDual` and `DualNum.atan()` (#44)
    This PR adds the `Polar2dDual` class, which will be used with Road
    Runner to calculate the position of the robot based on how far the
    wheels have rotated. (`TriWheels.inverse()`, but it uses dual numbers.)
    
    Because `Polar2dDual` requires converting to and from `Vector2dDual`, I
    have also implemented `atan()` (inverse tangent) for `DualNum` and a
    corresponding `atan2()` for `Vector2dDual`.
    
    For more information on what dual numbers are, see [On dual
    numbers](https://github.com/BotsBurgh/BOTSBURGH-FTC-2024-25/wiki/On-dual-numbers).

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Math.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Math.kt
@@ -1,9 +1,14 @@
 package org.firstinspires.ftc.teamcode.utils
 
+import com.acmerobotics.roadrunner.DualNum
 import com.acmerobotics.roadrunner.Vector2d
+import com.acmerobotics.roadrunner.Vector2dDual
 import kotlin.math.PI
+import kotlin.math.min
 import kotlin.math.pow
 import kotlin.math.round
+import kotlin.math.sign
+import kotlin.math.withSign
 
 /** PI / 2 */
 const val PI_2: Double = PI / 2.0
@@ -24,3 +29,76 @@ fun Double.roundDecimal(places: Int): Double {
 
 /** Applies function [f] to both [Vector2d.x] and [Vector2d.y], returning the result. */
 inline fun Vector2d.map(f: (Double) -> Double) = Vector2d(f(this.x), f(this.y))
+
+/** Returns the inverse tangent of a dual number. */
+fun <Param> DualNum<Param>.atan(): DualNum<Param> {
+    // Please see https://github.com/BotsBurgh/BOTSBURGH-FTC-2024-25/wiki/On-dual-numbers for an
+    // explanation of the code, and a guide on creating your own.
+
+    val x = this.values().toDoubleArray()
+    val y = DoubleArray(x.size)
+
+    if (y.isEmpty()) return y.toDual()
+
+    // Constant value
+    y[0] = kotlin.math.atan(x[0])
+    if (y.size == 1) return y.toDual()
+
+    // First derivative
+    val firstDeriv = 1 / (x[0].squared() + 1)
+    y[1] = firstDeriv * x[1]
+    if (y.size == 2) return y.toDual()
+
+    // Second derivative
+    val secondDeriv = (-2 * x[0]) / (x[0].squared() + 1).squared()
+    y[2] = secondDeriv * x[1] + firstDeriv * x[2]
+    if (y.size == 3) return y.toDual()
+
+    // Third derivative
+    val thirdDeriv = (6 * x[0].squared() - 2) / (x[0].squared() + 1).cubed()
+    y[3] = thirdDeriv * x[1] + secondDeriv * x[2] + secondDeriv * x[2] + firstDeriv * x[3]
+
+    return y.toDual()
+}
+
+/**
+ * Returns the inverse tangent of a [Vector2dDual].
+ *
+ * This corresponds to [kotlin.math.atan2], but does not implement special casing for infinity.
+ */
+fun <Param> Vector2dDual<Param>.atan2(): DualNum<Param> {
+    fun dmc(c: Double) = DualNum.constant<Param>(c, this.size)
+
+    val (x, y) = this.value()
+
+    // Special case for when the slope is 0.
+    if (y == 0.0) {
+        if (x == 0.0) return dmc(0.0)
+
+        return if (x > 0.0) {
+            dmc(0.0.withSign(y.sign))
+        } else {
+            dmc(PI.withSign(y.sign))
+        }
+    }
+
+    return (this.y / this.x).atan()
+}
+
+/**
+ * Converts a [DoubleArray] to a [DualNum].
+ *
+ * This is a temporary method until
+ * [road-runner#108](https://github.com/acmerobotics/road-runner/pull/108) is released.
+ */
+private fun <Param> DoubleArray.toDual() = DualNum<Param>(this.toList())
+
+/** Returns the minimum size of a [Vector2dDual]'s components. */
+private val <Param> Vector2dDual<Param>.size
+    get() = min(this.x.size(), this.y.size())
+
+/** Squares a number `x^2`. */
+private fun Double.squared() = this * this
+
+/** Cubes a number `x^3`. */
+private fun Double.cubed() = this * this * this
--- TeamCode/src/test/kotlin/org/firstinspires/ftc/teamcode/utils/MathTest.kt
+++ TeamCode/src/test/kotlin/org/firstinspires/ftc/teamcode/utils/MathTest.kt
@@ -1,8 +1,14 @@
 package org.firstinspires.ftc.teamcode.utils
 
+import com.acmerobotics.roadrunner.DualNum
+import com.acmerobotics.roadrunner.Time
 import com.acmerobotics.roadrunner.Vector2d
+import com.acmerobotics.roadrunner.Vector2dDual
+import kotlin.math.PI
+import kotlin.math.atan
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 internal class MathTest {
     @Test
@@ -23,5 +29,46 @@ internal class MathTest {
         val actual = Vector2d(2.0, 1.5).map { it * 2.0 }
 
         assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testDualAtanConsistency() {
+        val input = 7.5
+
+        val constantDual = DualNum.constant<Time>(input, 4)
+        val variableDual = DualNum.variable<Time>(input, 4)
+
+        // Test that at least the value implementation is correct, even if we can't easily test the
+        // derivatives.
+        assertEquals(atan(input), constantDual.atan().value())
+        assertEquals(atan(input), variableDual.atan().value())
+    }
+
+    // Derived from https://github.com/JetBrains/kotlin/blob/58392ba3f3f3e50891c91539d5c586ff4600743f/kotlin-native/runtime/test/numbers/MathTest.kt#L51.
+    @Test
+    fun testAtan2SpecialCases() {
+        // Creates a dual vector, calculates inverse tangent, then returns the value.
+        fun atan2Dual(
+            y: Double,
+            x: Double,
+        ) = Vector2dDual.constant<Time>(Vector2d(x, y), 4).atan2().value()
+
+        assertEquals(0.0, atan2Dual(0.0, 0.0))
+
+        assertEquals(0.0, atan2Dual(0.0, 1.0))
+        assertEquals(PI, atan2Dual(0.0, -1.0))
+        assertEquals(-0.0, atan2Dual(-0.0, 1.0))
+        assertEquals(-PI, atan2Dual(-0.0, -1.0))
+
+        // In the original test suite, edge cases for infinity were tested here. They have been
+        // skipped, since our implementation for `atan2()` does not support infinity.
+
+        assertEquals(PI / 2, atan2Dual(1.0, 0.0))
+        assertEquals(-PI / 2, atan2Dual(-1.0, 0.0))
+        assertEquals(PI / 2, atan2Dual(Double.POSITIVE_INFINITY, 1.0))
+        assertEquals(-PI / 2, atan2Dual(Double.NEGATIVE_INFINITY, 1.0))
+
+        assertTrue(atan2Dual(Double.NaN, 1.0).isNaN())
+        assertTrue(atan2Dual(1.0, Double.NaN).isNaN())
     }
 }
