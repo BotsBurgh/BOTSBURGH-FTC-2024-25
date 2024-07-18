b228ebc6105f9ac1031d5b73a4b587e9631c7b9a
BD103 <59022059+BD103@users.noreply.github.com>
2024-07-18 13:54:29 -0400

    feat: test `DualNum.atan()` by comparing it to `atan(Double)`

--- TeamCode/src/test/kotlin/org/firstinspires/ftc/teamcode/utils/MathTest.kt
+++ TeamCode/src/test/kotlin/org/firstinspires/ftc/teamcode/utils/MathTest.kt
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.utils
 
+import com.acmerobotics.roadrunner.DualNum
 import com.acmerobotics.roadrunner.Time
 import com.acmerobotics.roadrunner.Vector2d
 import com.acmerobotics.roadrunner.Vector2dDual
 import kotlin.math.PI
+import kotlin.math.atan
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
@@ -27,6 +29,19 @@ internal class MathTest {
         val actual = Vector2d(2.0, 1.5).map { it * 2.0 }
 
         assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testDualAtanConsistency() {
+        val input = 7.5
+
+        val constantDual = DualNum.constant<Time>(input, 4)
+        val variableDual = DualNum.variable<Time>(input, 4)
+
+        // Test that at least the value implementation is correct, even if we can't easily test the
+        // derivatives.
+        assertEquals(atan(input), constantDual.atan().value())
+        assertEquals(atan(input), variableDual.atan().value())
     }
 
     // Derived from https://github.com/JetBrains/kotlin/blob/58392ba3f3f3e50891c91539d5c586ff4600743f/kotlin-native/runtime/test/numbers/MathTest.kt#L51.
