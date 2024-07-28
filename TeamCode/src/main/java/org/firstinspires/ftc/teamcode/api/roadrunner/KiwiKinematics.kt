aebacaf788a398b27c4cd4b0fac6f102d64b614a
BD103 <59022059+BD103@users.noreply.github.com>
2024-07-28 14:51:47 -0400

    feat: wheel velocity constraints

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/roadrunner/KiwiKinematics.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/roadrunner/KiwiKinematics.kt
@@ -1,13 +1,18 @@
 package org.firstinspires.ftc.teamcode.api.roadrunner
 
+import com.acmerobotics.roadrunner.Arclength
 import com.acmerobotics.roadrunner.DualNum
+import com.acmerobotics.roadrunner.Pose2dDual
+import com.acmerobotics.roadrunner.PosePath
 import com.acmerobotics.roadrunner.PoseVelocity2dDual
 import com.acmerobotics.roadrunner.Twist2dDual
+import com.acmerobotics.roadrunner.VelConstraint
 import org.firstinspires.ftc.teamcode.api.TriWheels.BLUE_ANGLE
 import org.firstinspires.ftc.teamcode.api.TriWheels.GREEN_ANGLE
 import org.firstinspires.ftc.teamcode.api.TriWheels.RED_ANGLE
 import org.firstinspires.ftc.teamcode.utils.PI_2
 import org.firstinspires.ftc.teamcode.utils.Polar2dDual
+import kotlin.math.abs
 
 class KiwiKinematics(val radius: Double) {
     data class WheelTicks<Param>(
@@ -32,7 +37,9 @@ class KiwiKinematics(val radius: Double) {
         val red: DualNum<Param>,
         val green: DualNum<Param>,
         val blue: DualNum<Param>,
-    )
+    ) {
+        fun all() = listOf(this.red, this.green, this.blue)
+    }
 
     fun <Param> inverse(t: PoseVelocity2dDual<Param>): WheelVelocities<Param> {
         val polar = Polar2dDual.fromCartesian(t.linearVel)
@@ -46,5 +53,21 @@ class KiwiKinematics(val radius: Double) {
             polar.radius * (greenAngle - polar.theta) + t.angVel,
             polar.radius * (blueAngle - polar.theta) + t.angVel,
         )
+    }
+
+    inner class WheelVelConstraint(val maxWheelVel: Double) : VelConstraint {
+        override fun maxRobotVel(
+            robotPose: Pose2dDual<Arclength>,
+            path: PosePath,
+            s: Double,
+        ): Double {
+            val txRobotWorld = robotPose.value().inverse()
+            val robotVelWorld = robotPose.velocity().value()
+            val robotVelRobot = txRobotWorld * robotVelWorld
+
+            return this@KiwiKinematics.inverse(
+                PoseVelocity2dDual.constant<Arclength>(robotVelRobot, 1),
+            ).all().minOf { abs(maxWheelVel / it.value()) }
+        }
     }
 }
