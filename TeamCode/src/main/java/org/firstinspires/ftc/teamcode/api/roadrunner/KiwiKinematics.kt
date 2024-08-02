1bd0b0d2aac9ffbc8679bbc0abefed848c957e67
BD103 <59022059+BD103@users.noreply.github.com>
2024-08-01 20:34:40 -0400

    feat: add comments and finalization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/roadrunner/KiwiKinematics.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/roadrunner/KiwiKinematics.kt
@@ -2,11 +2,14 @@ package org.firstinspires.ftc.teamcode.api.roadrunner
 
 import com.acmerobotics.roadrunner.Arclength
 import com.acmerobotics.roadrunner.DualNum
+import com.acmerobotics.roadrunner.MecanumKinematics
 import com.acmerobotics.roadrunner.Pose2dDual
 import com.acmerobotics.roadrunner.PosePath
 import com.acmerobotics.roadrunner.PoseVelocity2dDual
+import com.acmerobotics.roadrunner.TankKinematics
 import com.acmerobotics.roadrunner.Twist2dDual
 import com.acmerobotics.roadrunner.VelConstraint
+import org.firstinspires.ftc.teamcode.api.TriWheels
 import org.firstinspires.ftc.teamcode.api.TriWheels.BLUE_ANGLE
 import org.firstinspires.ftc.teamcode.api.TriWheels.GREEN_ANGLE
 import org.firstinspires.ftc.teamcode.api.TriWheels.RED_ANGLE
@@ -14,25 +17,42 @@ import org.firstinspires.ftc.teamcode.utils.PI_2
 import org.firstinspires.ftc.teamcode.utils.Polar2dDual
 import kotlin.math.abs
 
-class KiwiKinematics(val radius: Double) {
+/**
+ * Used to compute the various physics related to the kiwi drive (Killough platform).
+ *
+ * This was based off of [MecanumKinematics] and [TankKinematics].
+ */
+class KiwiKinematics(private val radius: Double) {
+    /**
+     * Represents the amount of ticks each wheel on the drive rotated.
+     */
     data class WheelTicks<Param>(
         val red: DualNum<Param>,
         val green: DualNum<Param>,
         val blue: DualNum<Param>,
     )
 
+    /**
+     * Calculates the position and rotation of the robot based off of the wheel ticks.
+     */
     fun <Param> forward(w: WheelTicks<Param>): Twist2dDual<Param> {
-        // TODO: Find n property of kinematics dual numbers.
-        val r = Polar2dDual(DualNum.constant(RED_ANGLE - PI_2, 4), w.red).toCartesian()
-        val g = Polar2dDual(DualNum.constant(GREEN_ANGLE - PI_2, 4), w.green).toCartesian()
-        val b = Polar2dDual(DualNum.constant(BLUE_ANGLE - PI_2, 4), w.blue).toCartesian()
+        // Calculates the cartesian coordinates of each wheel, where the magnitude is the ticks.
+        // TODO: Find n property of kinematics dual numbers, instead of defaulting to 4.
+        val rxy = Polar2dDual(DualNum.constant(RED_ANGLE - PI_2, 4), w.red).toCartesian()
+        val gxy = Polar2dDual(DualNum.constant(GREEN_ANGLE - PI_2, 4), w.green).toCartesian()
+        val bxy = Polar2dDual(DualNum.constant(BLUE_ANGLE - PI_2, 4), w.blue).toCartesian()
 
         return Twist2dDual(
-            (r + g + b) / 1.5,
+            // Sum the cartesian coordinates and divide by 1.5 coefficient.
+            (rxy + gxy + bxy) / 1.5,
+            // Find rotation by summing linear distance and dividing by radius.
             (w.red + w.green + w.blue) / radius,
         )
     }
 
+    /**
+     * Represents the velocities of each wheel on the drive.
+     */
     data class WheelVelocities<Param>(
         val red: DualNum<Param>,
         val green: DualNum<Param>,
@@ -41,6 +61,11 @@ class KiwiKinematics(val radius: Double) {
         fun all() = listOf(this.red, this.green, this.blue)
     }
 
+    /**
+     * Calculates the velocity of each wheel from the linear and angular velocity of the robot.
+     *
+     * This is the dual-form of [TriWheels.compute].
+     */
     fun <Param> inverse(t: PoseVelocity2dDual<Param>): WheelVelocities<Param> {
         val polar = Polar2dDual.fromCartesian(t.linearVel)
 
@@ -55,7 +80,8 @@ class KiwiKinematics(val radius: Double) {
         )
     }
 
-    inner class WheelVelConstraint(val maxWheelVel: Double) : VelConstraint {
+    // Slightly modified from `MecanumKinematics.WheelVelConstraint`.
+    inner class WheelVelConstraint(private val maxWheelVel: Double) : VelConstraint {
         override fun maxRobotVel(
             robotPose: Pose2dDual<Arclength>,
             path: PosePath,
