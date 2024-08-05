346823b4537c3e0aa8bfdd3c9ecf860f26812ae7
BD103 <59022059+BD103@users.noreply.github.com>
2024-08-05 15:01:12 -0400

    feat: add some comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/roadrunner/KiwiLocalizer.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/roadrunner/KiwiLocalizer.kt
@@ -17,6 +17,11 @@ import org.firstinspires.ftc.teamcode.api.TriWheels
 import org.firstinspires.ftc.teamcode.core.API
 import org.firstinspires.ftc.teamcode.utils.RobotConfig
 
+/**
+ * An [API] used to detect where the robot is and how much it has moved.
+ *
+ * @see update
+ */
 object KiwiLocalizer : API() {
     override val dependencies = setOf(TriWheels)
 
@@ -58,32 +63,42 @@ object KiwiLocalizer : API() {
         this.kinematics = KiwiKinematics(RobotConfig.KiwiLocalizer.RADIUS)
     }
 
+    /**
+     * Returns a [Twist2dDual] representing how much the robot's position and heading has changed
+     * compared to the previous time this function was called.
+     */
     fun update(): Twist2dDual<Time> {
+        // Find position and velocity of each wheel.
         val redPosVel = this.red.getPositionAndVelocity()
         val greenPosVel = this.green.getPositionAndVelocity()
         val bluePosVel = this.blue.getPositionAndVelocity()
 
+        // Find heading of IMU.
         val angles = this.imu.robotYawPitchRollAngles
-
         val heading = Rotation2d.fromDouble(angles.getYaw(AngleUnit.RADIANS))
 
         if (this.firstUpdate) {
             this.firstUpdate = false
 
+            // Initialize last position and heading as current values.
             this.lastRedPos = redPosVel.position
             this.lastGreenPos = greenPosVel.position
             this.lastBluePos = bluePosVel.position
 
             this.lastHeading = heading
 
+            // Return a zeroed twist, equivalent no change.
             return Twist2dDual(
                 Vector2dDual.constant(Vector2d(0.0, 0.0), 2),
                 DualNum.constant(0.0, 2),
             )
         }
 
+        // Find the change in heading.
         val headingDelta = heading - this.lastHeading
 
+        // Calculate the twist: how much the robot has moved between the current and previous call
+        // to `update()`.
         val twist =
             this.kinematics.forward(
                 KiwiKinematics.WheelTicks(
@@ -108,12 +123,14 @@ object KiwiLocalizer : API() {
                 ),
             )
 
+        // Update last position and heading to current values.
         this.lastRedPos = redPosVel.position
         this.lastGreenPos = greenPosVel.position
         this.lastBluePos = bluePosVel.position
 
         this.lastHeading = heading
 
+        // Return the calculated twist, but replace the calculated angle with the one from the IMU.
         return Twist2dDual(twist.line, DualNum.cons(headingDelta, twist.angle.drop(1)))
     }
 }
