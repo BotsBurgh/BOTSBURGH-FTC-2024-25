6649c59b2790f0b3fa2eb1406b6140814dc75dcb
BD103 <59022059+BD103@users.noreply.github.com>
2024-08-11 13:42:39 -0400

    fix: `TeleOpMain` package

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpMain.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpMain.kt
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.teleOp
+package org.firstinspires.ftc.teamcode.teleop
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
