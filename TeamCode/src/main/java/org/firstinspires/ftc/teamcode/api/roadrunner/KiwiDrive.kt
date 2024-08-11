5d2db83806b872699e6a6ebec90eb2bbef1f68fa
BD103 <59022059+BD103@users.noreply.github.com>
2024-08-11 17:29:15 -0400

    feat: create constraints and finish action builder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/roadrunner/KiwiDrive.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/roadrunner/KiwiDrive.kt
@@ -1,13 +1,20 @@
 package org.firstinspires.ftc.teamcode.api.roadrunner
 
+import com.acmerobotics.roadrunner.AngularVelConstraint
 import com.acmerobotics.roadrunner.HolonomicController
+import com.acmerobotics.roadrunner.MinVelConstraint
 import com.acmerobotics.roadrunner.MotorFeedforward
 import com.acmerobotics.roadrunner.Pose2d
 import com.acmerobotics.roadrunner.PoseVelocity2d
+import com.acmerobotics.roadrunner.ProfileAccelConstraint
+import com.acmerobotics.roadrunner.ProfileParams
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder
+import com.acmerobotics.roadrunner.TrajectoryBuilderParams
+import com.acmerobotics.roadrunner.TurnConstraints
 import org.firstinspires.ftc.teamcode.api.TriWheels
 import org.firstinspires.ftc.teamcode.api.Voltage
 import org.firstinspires.ftc.teamcode.core.API
+import org.firstinspires.ftc.teamcode.utils.RobotConfig
 
 object KiwiDrive : API() {
     override val dependencies = setOf(TriWheels, KiwiLocalizer, Voltage)
@@ -17,16 +24,52 @@ object KiwiDrive : API() {
     val controller: HolonomicController = TODO()
     val feedforward: MotorFeedforward = TODO()
 
-    fun actionBuilder(beginPose: Pose2d) = TrajectoryActionBuilder(
-        ::TurnAction,
-        ::FollowTrajectoryAction,
-        TODO("trajectory build params"),
-        beginPose,
-        0.0,
-        TODO("turn constraints"),
-        TODO("vel constraints"),
-        TODO("accel constraints"),
-    )
+    // This was taken directly from `MecanumDrive`. It shouldn't be changed unless you know what
+    // you're doing.
+    private val trajectoryBuilderParams =
+        TrajectoryBuilderParams(
+            arcLengthSamplingEps = 1e-6,
+            ProfileParams(
+                dispResolution = 0.25,
+                angResolution = 0.1,
+                angSamplingEps = 1e-2,
+            ),
+        )
+
+    // TODO: Make constraints update with FTC Dashboard.
+    private val turnConstraint =
+        TurnConstraints(
+            RobotConfig.KiwiDrive.MAX_ANG_VEL,
+            -RobotConfig.KiwiDrive.MAX_ANG_ACCEL,
+            RobotConfig.KiwiDrive.MAX_ANG_ACCEL,
+        )
+
+    private val velConstraint =
+        MinVelConstraint(
+            listOf(
+                KiwiLocalizer.kinematics.WheelVelConstraint(RobotConfig.KiwiDrive.MAX_WHEEL_VEL),
+                AngularVelConstraint(RobotConfig.KiwiDrive.MAX_ANG_VEL),
+            ),
+        )
+
+    private val accelConstraint =
+        ProfileAccelConstraint(
+            RobotConfig.KiwiDrive.MIN_PROFILE_ACCEL,
+            RobotConfig.KiwiDrive.MAX_PROFILE_ACCEL,
+        )
+
+    fun actionBuilder(beginPose: Pose2d) =
+        TrajectoryActionBuilder(
+            ::TurnAction,
+            ::FollowTrajectoryAction,
+            this.trajectoryBuilderParams,
+            beginPose,
+            // Robot should end stopped.
+            beginEndVel = 0.0,
+            this.turnConstraint,
+            this.velConstraint,
+            this.accelConstraint,
+        )
 
     /** Updates the currently tracked [pose] using [KiwiLocalizer]. */
     fun updatePoseEstimates(): PoseVelocity2d {
