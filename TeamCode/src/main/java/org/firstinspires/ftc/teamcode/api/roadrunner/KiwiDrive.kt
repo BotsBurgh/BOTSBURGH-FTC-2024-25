05ea603d620fe17c81da56e15c7eaf2f26113c9a
BD103 <59022059+BD103@users.noreply.github.com>
2024-08-11 15:04:13 -0400

    feat: begin action build

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/roadrunner/KiwiDrive.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/roadrunner/KiwiDrive.kt
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.api.roadrunner
 
 import com.acmerobotics.roadrunner.Pose2d
 import com.acmerobotics.roadrunner.PoseVelocity2d
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder
 import org.firstinspires.ftc.teamcode.api.TriWheels
 import org.firstinspires.ftc.teamcode.api.Voltage
 import org.firstinspires.ftc.teamcode.core.API
@@ -10,6 +11,17 @@ object KiwiDrive : API() {
     override val dependencies = setOf(TriWheels, KiwiLocalizer, Voltage)
 
     var pose = Pose2d(0.0, 0.0, 0.0)
+
+    fun actionBuilder(beginPose: Pose2d) = TrajectoryActionBuilder(
+        ::TurnAction,
+        ::FollowTrajectoryAction,
+        TODO("trajectory build params"),
+        beginPose,
+        0.0,
+        TODO("turn constraints"),
+        TODO("vel constraints"),
+        TODO("accel constraints"),
+    )
 
     /** Updates the currently tracked [pose] using [KiwiLocalizer]. */
     fun updatePoseEstimates(): PoseVelocity2d {
