3099df0dc3bb9fb7db6304b73712d2b13196e947
BD103 <59022059+BD103@users.noreply.github.com>
2024-09-04 20:25:53 -0400

    feat: working demo of encoder localization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/demos/EncoderLocalizationDemo.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/demos/EncoderLocalizationDemo.kt
@@ -2,19 +2,28 @@ package org.firstinspires.ftc.teamcode.teleop.demos
 
 import com.acmerobotics.dashboard.FtcDashboard
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket
+import com.acmerobotics.roadrunner.Rotation2d
+import com.acmerobotics.roadrunner.Twist2d
+import com.acmerobotics.roadrunner.Vector2d
 import com.qualcomm.robotcore.eventloop.opmode.Disabled
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.firstinspires.ftc.teamcode.api.TriWheels
+import org.firstinspires.ftc.teamcode.api.TriWheels.RED_ANGLE
+import org.firstinspires.ftc.teamcode.api.TriWheels.GREEN_ANGLE
+import org.firstinspires.ftc.teamcode.api.TriWheels.BLUE_ANGLE
+import org.firstinspires.ftc.teamcode.utils.PI_2
 import org.firstinspires.ftc.teamcode.utils.Polar2d
 import kotlin.math.PI
 
 @TeleOp(name = "Encoder Localization Demo", group = "Demo")
-@Disabled
+//@Disabled
 class EncoderLocalizationDemo : OpMode() {
     private val driveSpeed = 0.3
-    private val wheelDiameter = 3.8
-    private val ticksPerRevolution = 145.1
+    private val wheelDiameter = 12.0
+    private val inchesPerTick = 0.082191780821918
+
+    private val wheelRadius = this.wheelDiameter / 2.0
 
     override fun init() {
         TriWheels.init(this)
@@ -36,13 +45,13 @@ class EncoderLocalizationDemo : OpMode() {
         // Find positions of each wheel, converting to distance traveled in inches.
         val wheelPositions =
             Triple(
-                this.ticksToInch(TriWheels.red.currentPosition),
-                this.ticksToInch(TriWheels.green.currentPosition),
-                this.ticksToInch(TriWheels.blue.currentPosition),
+                TriWheels.red.currentPosition.toDouble() * this.inchesPerTick,
+                TriWheels.green.currentPosition.toDouble() * this.inchesPerTick,
+                TriWheels.blue.currentPosition.toDouble() * this.inchesPerTick,
             )
 
         // Find position of the entire robot.
-        val robotPosition = TriWheels.inverse(wheelPositions)
+        val (robotPosition, robotRotation) = this.inverse(wheelPositions)
 
         val packet = TelemetryPacket()
 
@@ -54,13 +63,24 @@ class EncoderLocalizationDemo : OpMode() {
 
             f("X", robotPosition.x)
             f("Y", robotPosition.y)
+            f("Rot", robotRotation)
         }
 
         // Draw robot for FTC Dashboard.
         packet.fieldOverlay().apply {
             setStroke("#3F51B5")
             setStrokeWidth(1)
-            strokeCircle(robotPosition.x, robotPosition.y, 9.0)
+
+            val pos = Vector2d(robotPosition.y, -robotPosition.x)
+
+            // Swap X and Y because field coordinate is
+            strokeCircle(pos.x, pos.y, this@EncoderLocalizationDemo.wheelRadius)
+
+            val halfv = Rotation2d.exp(robotRotation).vec().times(0.5 * this@EncoderLocalizationDemo.wheelRadius)
+            val p1 = pos + halfv
+            val p2 = p1 + halfv
+
+            strokeLine(p1.x, p1.y, p2.x, p2.y)
         }
 
         FtcDashboard.getInstance().sendTelemetryPacket(packet)
@@ -68,5 +88,14 @@ class EncoderLocalizationDemo : OpMode() {
         telemetry.addData("Status", "Running")
     }
 
-    private fun ticksToInch(position: Int) = position.toDouble() / this.ticksPerRevolution * this.wheelDiameter * PI
+    private fun inverse(ticks: Triple<Double, Double, Double>): Twist2d {
+        val rxy = Polar2d(RED_ANGLE - PI_2, ticks.first).toCartesian()
+        val gxy = Polar2d(GREEN_ANGLE - PI_2, ticks.second).toCartesian()
+        val bxy = Polar2d(BLUE_ANGLE - PI_2, ticks.third).toCartesian()
+
+        return Twist2d(
+            (rxy + gxy + bxy) / 1.5,
+            (ticks.first + ticks.second + ticks.third) / this.wheelRadius / 3.0,
+        )
+    }
 }
