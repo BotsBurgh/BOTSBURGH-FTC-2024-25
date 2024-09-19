2da008663cc450dad27a9692048092e9f477a01c
Vifi5 <117041326+Vif15@users.noreply.github.com>
2024-09-18 20:30:51 -0400

    quadWheel Movement (untested)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/QuadWheel.kt
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode.api
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorEx
+import com.qualcomm.robotcore.hardware.DcMotorSimple
+import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1
+import org.firstinspires.ftc.teamcode.api.TriWheels.stop
+import org.firstinspires.ftc.teamcode.core.API
+
+
+object QuadWheels: API() {
+    lateinit var bl: DcMotorEx
+        private set
+    lateinit var br: DcMotorEx
+        private set
+    lateinit var  fr: DcMotorEx
+        private set
+    lateinit var fl: DcMotorEx
+        private set
+
+    val y = -gamepad1.left_stick_y.toDouble()
+    val x = gamepad1.left_stick_x.toDouble()
+    val rx = gamepad1.right_stick_x.toDouble()
+
+
+    override fun init(opMode:OpMode) {
+        super.init(opMode)
+
+        bl = this.opMode.hardwareMap.get(DcMotorEx::class.java, "backLeft")
+        br = this.opMode.hardwareMap.get(DcMotorEx::class.java, "backLeft")
+        fr = this.opMode.hardwareMap.get(DcMotorEx::class.java, "backLeft")
+        fl = this.opMode.hardwareMap.get(DcMotorEx::class.java, "backLeft")
+
+        stopAndResetMotors()
+    }
+
+    /**
+     * Sets the power of each wheel respectively.
+     */
+    fun power(
+        blPower: Double,
+        brPower: Double,
+        frPower: Double,
+        flPower: Double,
+    ) {
+        bl.power = blPower
+        br.power = brPower
+        fr.power = frPower
+        fl.power = flPower
+    }
+
+    fun drive() {
+        power((y - x + rx), (y + x - rx), (y - x - rx), (y + x + rx))
+    }
+
+    fun stopAndResetMotors() {
+        stop()
+
+        for (motor in arrayOf(bl, br, fr, fl)) {
+            motor.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+            motor.mode = DcMotor.RunMode.RUN_USING_ENCODER
+            motor.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+            motor.direction = DcMotorSimple.Direction.FORWARD
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpMain.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpMain.kt
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.teleop
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.firstinspires.ftc.teamcode.RobotConfig
+import org.firstinspires.ftc.teamcode.api.QuadWheels
 import org.firstinspires.ftc.teamcode.api.TriWheels
 import kotlin.math.PI
 import kotlin.math.atan2
@@ -11,26 +12,11 @@ import kotlin.math.sqrt
 @TeleOp(name = "Main")
 class TeleOpMain : OpMode() {
     override fun init() {
-        TriWheels.init(this)
+        QuadWheels.init(this)
     }
 
     override fun loop() {
-        // joystick input
-        val joyX = -this.gamepad1.left_stick_x.toDouble()
-        val joyY = this.gamepad1.left_stick_y.toDouble()
-
-        // PI / 3 because 0 radians is right, not forward
-        val joyRadians = atan2(joyY, joyX) - (PI / 3.0) - (2.0 * PI / 3.0)
-
-        val joyMagnitude = sqrt(joyY * joyY + joyX * joyX)
-
-        val rotationPower = -this.gamepad1.right_stick_x.toDouble()
-
         // movement of all wheels
-        TriWheels.drive(
-            joyRadians,
-            joyMagnitude * RobotConfig.TeleOpMain.DRIVE_SPEED,
-            rotation = rotationPower * RobotConfig.TeleOpMain.ROTATE_SPEED,
-        )
+        QuadWheels.drive()
     }
 }
