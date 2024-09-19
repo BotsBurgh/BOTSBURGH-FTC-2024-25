c94d4c5d485e447645c3f5b4de143e79d815cd45
Vifi5 <117041326+Vif15@users.noreply.github.com>
2024-09-18 20:41:26 -0400

    quadWheel Movement (untested)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpMain.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpMain.kt
@@ -2,12 +2,8 @@ package org.firstinspires.ftc.teamcode.teleop
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
-import org.firstinspires.ftc.teamcode.RobotConfig
 import org.firstinspires.ftc.teamcode.api.QuadWheels
-import org.firstinspires.ftc.teamcode.api.TriWheels
-import kotlin.math.PI
-import kotlin.math.atan2
-import kotlin.math.sqrt
+
 
 @TeleOp(name = "Main")
 class TeleOpMain : OpMode() {
