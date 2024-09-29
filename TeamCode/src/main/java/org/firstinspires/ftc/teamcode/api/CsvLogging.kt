a37e10da9505bd86e45333e07eab40c1464ffa34
BD103 <59022059+BD103@users.noreply.github.com>
2024-09-29 17:24:39 -0400

    chore!: remove volatile files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/CsvLogging.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/CsvLogging.kt
@@ -16,7 +16,6 @@ object CsvLogging : API() {
     private val BOTSBURGH_FOLDER = File(AppUtil.ROOT_FOLDER, "/BotsBurgh")
 
     private val fileHash = hashMapOf<String, BufferedWriter>()
-    private val volatileFileHash = hashMapOf<String, File>()
 
     override fun init(opMode: OpMode) {
         super.init(opMode)
@@ -35,16 +34,6 @@ object CsvLogging : API() {
     fun createFile(fileName: String) {
         if (RobotConfig.DEBUG) {
             fileHash[fileName] = BufferedWriter(FileWriter(File(BOTSBURGH_FOLDER, "/$fileName.csv"), true))
-            File(BOTSBURGH_FOLDER, "/$fileName.csv").createNewFile()
-        }
-    }
-
-    /**
-     * Creates new volatile files to log to (old files are deleted after every run). Only use volatile files to diagnose power issues.
-     */
-    fun createVolatileFile(fileName: String) {
-        if (RobotConfig.DEBUG) {
-            volatileFileHash[fileName] = File(BOTSBURGH_FOLDER, "/$fileName.csv")
             File(BOTSBURGH_FOLDER, "/$fileName.csv").createNewFile()
         }
     }
@@ -82,39 +71,8 @@ object CsvLogging : API() {
     }
 
     /**
-     * Writes Double data to the targeted volatile file
-     * @param file Name of the file that is being logged to
-     * @param data Double data that is being logged
-     */
-    fun writeVolatileFile(
-        file: String,
-        data: Double,
-    ) {
-        if (RobotConfig.DEBUG) {
-            fileHash[file]!!.write("$data, ")
-            fileHash[file]!!.write(opMode.runtime.toString())
-            fileHash[file]!!.newLine()
-        }
-    }
-
-    /**
-     * Writes Array Double data to the targeted volatile file
-     * @param file Name of the file that is being logged to
-     * @param data Array Double data that is being logged
-     */
-    fun writeVolatileFile(
-        file: String,
-        data: Array<Double>,
-    ) {
-        if (RobotConfig.DEBUG) {
-            for (i in data) fileHash[file]!!.write("$i, ")
-        }
-        fileHash[file]!!.write(opMode.runtime.toString())
-        fileHash[file]!!.newLine()
-    }
-
-    /**
-     * Closes file. Only use on non-volatile files
+     * Closes file.
+     *
      * @param file Name of file to close
      */
     fun close(file: String) {
