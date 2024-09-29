0d49135e15a8477cf8703710a983f251db47f7e4
Imthebiggestbird2 <joshbirendra@gmail.com>
2024-09-29 17:52:41 -0400

    Initial tensorflow push -Added a Scan API thats based off of the sample tf class
    -Added the TFlite file for CenterStage(Will replace later)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/ScanAPI.kt
@@ -0,0 +1,128 @@
+package tensorflow
+
+import com.google.blocks.ftcrobotcontroller.util.CurrentGame.TFOD_MODEL_ASSET
+import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap
+import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry
+import org.firstinspires.ftc.teamcode.core.API
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName
+import org.firstinspires.ftc.vision.VisionPortal
+import org.firstinspires.ftc.vision.tfod.TfodProcessor
+import tensorflow.ScanAPI.Define.USE_WEBCAM
+
+
+
+
+object ScanAPI : API() {
+    /**
+     * The variable to store our instance of the TensorFlow Object Detection processor.
+     */
+    private var tfod: TfodProcessor? = null
+
+    /**
+     * The variable to store our instance of the vision portal.
+     */
+    private var visionPortal: VisionPortal? = null
+
+    /**
+     * Initialize the TensorFlow Object Detection processor.
+     */
+    private fun initTfod() {
+        // Create the TensorFlow processor by using a builder.
+
+        tfod =
+            TfodProcessor.Builder() // With the following lines commented out, the default TfodProcessor Builder
+                // will load the default model for the season. To define a custom model to load,
+                // choose one of the following:
+                //   Use setModelAssetName() if the custom TF Model is built in as an asset (AS only).
+                //   Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
+
+                .setModelAssetName(TFOD_MODEL_ASSET) //.setModelFileName(TFOD_MODEL_FILE)
+                // The following default settings are available to un-comment and edit as needed to
+                // set parameters for custom models.
+                //.setModelLabels(LABELS)
+                //.setIsModelTensorFlow2(true)
+                //.setIsModelQuantized(true)
+                //.setModelInputSize(300)
+                //.setModelAspectRatio(16.0 / 9.0)
+
+                .build()
+
+        // Create the vision portal by using a builder.
+        val builder = VisionPortal.Builder()
+
+        // Set the camera (webcam vs. built-in RC phone camera).
+        if (USE_WEBCAM) {
+            builder.setCamera(hardwareMap.get(WebcamName::class.java, "Webcam 1"))
+        } else {
+            builder.setCamera(BuiltinCameraDirection.BACK)
+        }
+
+        // Choose a camera resolution. Not all cameras support all resolutions.
+        //builder.setCameraResolution(new Size(640, 480));
+
+        // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
+        builder.enableLiveView(false);
+
+        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
+        //builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
+
+        // Choose whether or not LiveView stops if no processors are enabled.
+        // If set "true", monitor shows solid orange screen if no processors enabled.
+        // If set "false", monitor shows camera view without annotations.
+        //builder.setAutoStopLiveView(false);
+
+        // Set and enable the processor.
+        builder.addProcessor(tfod)
+
+        // Build the Vision Portal, using the above settings.
+        visionPortal = builder.build()
+
+        // Set confidence threshold for TFOD recognitions, at any time.
+        //tfod.setMinResultConfidence(0.75f);
+
+        // Disable or re-enable the TFOD processor at any time.
+        //visionPortal.setProcessorEnabled(tfod, true);
+    } // end method initTfod()
+
+    /**
+     * Add telemetry about TensorFlow Object Detection (TFOD) recognitions.
+     */
+    private fun telemetryTfod() {
+        val currentRecognitions = tfod!!.recognitions
+        telemetry.addData("# Objects Detected", currentRecognitions.size)
+
+        // Step through the list of recognitions and display info for each one.
+        for (recognition in currentRecognitions) {
+            val x = ((recognition.left + recognition.right) / 2).toDouble()
+            val y = ((recognition.top + recognition.bottom) / 2).toDouble()
+
+            telemetry.addData("", " ")
+            telemetry.addData(
+                "Image",
+                "%s (%.0f %% Conf.)",
+                recognition.label,
+                recognition.confidence * 100
+            )
+            telemetry.addData("- Position", "%.0f / %.0f", x, y)
+            telemetry.addData("- Size", "%.0f x %.0f", recognition.width, recognition.height)
+        } // end for() loop
+    } // end method telemetryTfod()
+
+    object Define{
+        const val USE_WEBCAM = true // true for webcam, false for phone camera
+
+        // TFOD_MODEL_ASSET points to a model file stored in the project Asset location,
+        // this is only used for Android Studio when using models in Assets.
+        private const val TFOD_MODEL_ASSET = "CenterStage.tflite"
+
+        // Define the labels recognized in the model for TFOD (must be in training order!)
+        private val LABELS = arrayOf(
+            "Pixel",
+        )
+    }
+
+    fun scan(){
+        visionPortal!!.setProcessorEnabled(tfod, true);
+    }
+} // end class
