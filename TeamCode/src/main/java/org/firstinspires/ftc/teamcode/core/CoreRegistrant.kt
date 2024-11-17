fc095e54f19ee2b9e77e275e06817759e1071e37
BD103 <59022059+BD103@users.noreply.github.com>
2024-11-17 16:46:03 -0500

    feat: document `CoreRegistrant` class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/CoreRegistrant.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/CoreRegistrant.kt
@@ -7,29 +7,45 @@ import com.qualcomm.robotcore.eventloop.opmode.OpModeManagerImpl
 import com.qualcomm.robotcore.eventloop.opmode.OpModeManagerNotifier
 import org.firstinspires.ftc.ftccommon.external.OnCreateEventLoop
 
+/**
+ * This object is responsible for registering and running the core systems required for all opmodes.
+ *
+ * Its method [register] adds this object as a listener which then gets called when certain opmode
+ * actions occur, as specified in [OpModeManagerNotifier]. So far, this object facilitates:
+ *
+ * - Resetting all [Resettable] variables at the start of an opmode.
+ * - Setting up [Logging], and closing it after an opmode finishes.
+ * - Checking [Dependencies] when an opmode is started.
+ */
 object CoreRegistrant : OpModeManagerNotifier.Notifications {
-    private val log = Logging.Logger(this)
-
     override fun onOpModePreInit(opMode: OpMode) {
+        // Reset all `Resettable` variables. This *must* happen before anything else, or old
+        // values will leak into the program.
         Resettable.resetAll()
 
+        // Initialize logging if a user-defined opmode is being run. (The `DefaultOpMode`, which is
+        // run when no other opmode is running, is skipped.)
         if (opMode !is OpModeManagerImpl.DefaultOpMode) {
             Logging.init(opMode)
         }
     }
 
     override fun onOpModePreStart(opMode: OpMode) {
+        // Check dependencies when running a user-defined opmode.
         if (opMode !is OpModeManagerImpl.DefaultOpMode) {
             Dependencies.checkDependencies()
         }
     }
 
     override fun onOpModePostStop(opMode: OpMode) {
+        // Close and save the log file when running a user-defined opmode. This will crash if run
+        // for `DefaultOpMode`, as `Logging.close()` assumes a log file has already been created.
         if (opMode !is OpModeManagerImpl.DefaultOpMode) {
             Logging.close()
         }
     }
 
+    // This is an entrypoint that is called once at the beginning of the entire robot's lifecycle.
     @OnCreateEventLoop
     @JvmStatic
     fun register(
