75ba9fb7f7b9af3c5bb7f5cfbb030acaef84d874
BD103 <59022059+BD103@users.noreply.github.com>
2024-11-17 16:26:28 -0500

    fix: do not initialize logs or check dependencies for default opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/CoreRegistrant.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/CoreRegistrant.kt
@@ -3,21 +3,31 @@ package org.firstinspires.ftc.teamcode.core
 import android.content.Context
 import com.qualcomm.ftccommon.FtcEventLoop
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.OpModeManagerImpl
 import com.qualcomm.robotcore.eventloop.opmode.OpModeManagerNotifier
 import org.firstinspires.ftc.ftccommon.external.OnCreateEventLoop
 
 object CoreRegistrant : OpModeManagerNotifier.Notifications {
+    private val log = Logging.Logger(this)
+
     override fun onOpModePreInit(opMode: OpMode) {
         Resettable.resetAll()
-        Logging.init(opMode)
+
+        if (opMode !is OpModeManagerImpl.DefaultOpMode) {
+            Logging.init(opMode)
+        }
     }
 
     override fun onOpModePreStart(opMode: OpMode) {
-        Dependencies.checkDependencies()
+        if (opMode !is OpModeManagerImpl.DefaultOpMode) {
+            Dependencies.checkDependencies()
+        }
     }
 
     override fun onOpModePostStop(opMode: OpMode) {
-        Logging.close()
+        if (opMode !is OpModeManagerImpl.DefaultOpMode) {
+            Logging.close()
+        }
     }
 
     @OnCreateEventLoop
